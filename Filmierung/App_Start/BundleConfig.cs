de88a4ec8d42453bbf160c8208820245e5e2bd6a
patarok <patoschkapeter@gmail.com>
2020-01-12 19:39:19 +0100

    all good

--- Filmierung/App_Start/BundleConfig.cs
+++ Filmierung/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace Filmierung
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap_sketchy.css",
                       "~/Content/site.css"));
         }
     }
