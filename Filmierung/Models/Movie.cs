fbd9830d9b74fe1d5bf2d52f32dad932a1eb809b
patarok <patoschkapeter@gmail.com>
2020-05-23 18:55:08 +0200

    asides fertiggestellt. TODO: Einbindung Video-Diagramm eventuell Code-Highlighting und Erweiterung der Funktionalitaeten sowie "FAQ"-MenueEintrag in der Naviagtionsleiste mit einem Punkt auf der Seite lautend: 'FAQ: How-do-I-work.'

--- Filmierung/Models/Movie.cs
+++ Filmierung/Models/Movie.cs
@@ -7,14 +7,12 @@ namespace Filmierung.Models
 {
     public class Movie
     {
-
         public int Id { get; set; }
         public string Title { get; set; }
         public string Genre { get; set; }
-        public int Rating { get; set; }
         public bool Aktiv { get; set; }
         public int PriceId { get; set; }
-
+        public int Rating { get; set; }
         // Change V02
         public bool Ab18 { get; set; }
 
